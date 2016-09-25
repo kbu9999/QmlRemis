5c79c1991df85b80bb8af6e0b457afd053f58dd6
kbu9999 <kbu9999@gmail.com>
2016-09-25 17:54:56 -0300

    buildpackage

---
+++ buildpackage.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+#create package
+
+sudo checkinstall --pkgname qmlremis -pkgversion 0.2 --requires asterisk,mysql-server,openssh-server,libqt5qml5,libqt5sql5-mysql,qml-module-qtquick-controls,qml-module-qtgraphicaleffects,qml-module-qtwebsockets,qml-module-qtlocation,qml-module-qtpositioning,qml-module-qt-labs-settings,python-starpy,python-sqlalchemy,python-twisted,python-autobahn,python-pymysql,python-yapsy --maintainer kbu9999@gmail.com -y
